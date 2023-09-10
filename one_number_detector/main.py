7e9936826a80b5383c0d7337b5632d25c52fd4db
Vadim <V.gorb-123456789@yandex.ru>
2023-09-10 16:32:15 +0300

    Unworking training added ;)

--- one_number_detector/main.py
+++ one_number_detector/main.py
@@ -1,7 +1,205 @@
-import numpy as np
+import PySimpleGUI as sg
+import neural_structs as ns
 
-a = [[1, 2, 3],
-     [4, 5, 6],
-     [7, 8, 9]]
+sg.theme('Dark Blue 3')
 
-print(np.transpose(a))
+
+#адаптер к холсту
+class DrawGrid():
+    def __init__(self, width, height, h_resolution) -> None:
+        self.width: int = width
+        self.height: int = height
+        self.h_resolution = h_resolution
+        self.grid_step: int = round(self.width/self.h_resolution)
+        self.gwidth = self.width//self.grid_step
+        self.gheight = self.height//self.grid_step
+        #холст
+        self.area: sg.Graph = sg.Graph(canvas_size=(self.width, self.height), 
+                     graph_bottom_left=(0, self.height), 
+                     graph_top_right=(self.width, 0), 
+                     background_color='white',
+                     key='-INPUT-',
+                     enable_events=True,
+                     drag_submits=True)
+        self.objects = []
+        self.binary_set: list(int) = [0 for i in range(0, self.gwidth*self.gheight)]
+
+
+    def draw_grid(self):
+        #Сетка для разбиения на "пиксели"
+        work_grid_vert = [self.area.draw_line((x, 0), (x, self.height), color='grey') for x in range(0, self.width, self.grid_step)]
+        work_grid_hor = [self.area.draw_line((0, y), (self.width, y), color='grey') for y in range(0, self.height, self.grid_step)]
+
+
+    def clear(self):
+        for obj in self.objects:
+            self.area.delete_figure(obj)
+            self.binary_set = [0 for i in range(0, self.gwidth*self.gheight)]
+
+
+    def draw(self, x, y):
+        gx = (x//self.grid_step)
+        gy = (y//self.grid_step)
+        if self.binary_set[gy*self.gwidth + gx] != 0:
+            return
+        rx = gx*self.grid_step
+        ry = gy*self.grid_step
+        self.objects.append(self.area.draw_rectangle((rx, ry), (rx+self.grid_step, ry+self.grid_step), fill_color='black'))
+        self.binary_set[gy*self.gwidth + gx] = 1
+
+
+
+class PresentationWindow():
+    def __init__(self) -> None:
+        self.grid = DrawGrid(500, 700, 5)
+
+        #размещаем нужные элементы в окне
+        self.presentation_layout = [
+            [sg.Text('Нарисуйте цифру'), sg.Text(size=(12,1))],
+            [self.grid.area],
+            [sg.Text('Это выглядит как: ', key='-OUT-')],
+            [sg.Button('Clear'), sg.Button('Exit')]]
+        self.presentation_window = sg.Window('Work Presentation', self.presentation_layout, finalize=True)
+
+        #создаём сетку для рисования
+        self.grid.draw_grid()
+
+
+    def update_answer(self, answer: int):
+        self.presentation_window['-OUT-'].update('Это выглядит как: {}'.format(answer))
+
+
+    def open(self):
+        while True:  # Event Loop
+            event, values = self.presentation_window.read()
+            print(event, values)
+            if event == sg.WIN_CLOSED or event == 'Exit':
+                break
+            if event == '-INPUT-':
+                # window['-OUT-'].update("Это выглядит как: ")
+                self.grid.draw(*values['-INPUT-'])
+            if event == 'Clear':
+                self.grid.clear()
+            if event == '-INPUT-+UP':
+                self.update_answer(values['-INPUT-'][0])
+
+        self.presentation_window.close()
+
+
+
+class GenerateWindow():
+    def __init__(self) -> None:
+        self.grid = DrawGrid(500, 700, 5)
+
+        #размещаем нужные элементы в окне
+        self.presentation_layout = [
+            [sg.Text('Нарисуйте цифру'), sg.Text(size=(12,1))],
+            [self.grid.area],
+            [sg.Button('Сохранить набор данных для цифры: ', key="-SAVE-"), 
+             sg.Combo(['1', '2', '3', '4', '4', '6', '7', '8', '9', '0'], key="-DATASET_REFERENCE-")],
+            [sg.Button('Clear'), sg.Button('Exit')]]
+        self.presentation_window = sg.Window('Work Presentation', self.presentation_layout, finalize=True)
+
+        #создаём сетку для рисования
+        self.grid.draw_grid()
+
+
+    def save_dataset(self, target: int, dataset: list[int]):
+        zipdata = '|'.join([str(i) for i in dataset]) + '>{}\n'.format(target)
+        with open("ond_dataset_{}rsl.txt".format(self.grid.h_resolution), "a") as f:
+            f.writelines(zipdata)
+
+
+    def open(self):
+        while True:  # Event Loop
+            event, values = self.presentation_window.read()
+            print(event, values)
+            if event == sg.WIN_CLOSED or event == 'Exit':
+                break
+            if event == '-INPUT-':
+                # window['-OUT-'].update("Это выглядит как: ")
+                self.grid.draw(*values['-INPUT-'])
+            if event == 'Clear':
+                self.grid.clear()
+            if event == '-SAVE-':
+                self.save_dataset(values['-DATASET_REFERENCE-'], self.grid.binary_set)
+
+        self.presentation_window.close()
+
+
+
+def dataset_target_decode(target: int, neurons_count: int):
+    v = [0 for i in range(neurons_count)]
+    v[target-1] = 1
+    return v
+
+
+
+def dataset_decoder(path: str, neurons_count: int):
+    with open(path, 'r') as f:
+        data = f.read()
+    lined_data = data.split('\n')
+    lined_data.pop()
+    return [[[int(x) for x in set[0].split('|')], dataset_target_decode(int(set[1]), neurons_count)] for set in [line.split('>') for line in lined_data]]
+
+
+
+# class TrainWindow():
+#     def __init__(self) -> None:
+#         self.width = 500
+#         self.height = 300
+#         self.stat = []
+#         self.last_point = 0
+#         self.graph_area: sg.Graph = sg.Graph(canvas_size=(self.width, self.height), 
+#                      graph_bottom_left=(0, 0), 
+#                      graph_top_right=(self.width, self.height), 
+#                      background_color='white',
+#                      key='-GRAPH-')
+
+#         #размещаем нужные элементы в окне
+#         self.presentation_layout = [
+#             [sg.Text('Обучение')],
+#             [sg.T('Выберите файл с данными для обучения:'), sg.FileBrowse(button_text="Выбрать", key='-FILE-')],
+#             [sg.Text('Отклонение от тестового набора:')],
+#             [self.graph_area],
+#             [sg.Button('Начать обучение', key='-START-'), sg.Button('Остановить обучение', key='-STOP-'), sg.Button('Exit')]]
+#         self.presentation_window = sg.Window('Work Presentation', self.presentation_layout, finalize=True)
+
+
+#     def _upd_stat(self, d: int):
+#         point = d
+#         print(point)
+#         step = 5
+#         [self.graph_area.move_figure(s, -step, 0) for s in self.stat]
+#         self.graph_area.draw_line((self.width-step, self.last_point), (self.width, point))
+#         self.last_point = point
+
+
+#     def open(self):
+#         while True:  # Event Loop
+#             event, values = self.presentation_window.read()
+#             print(event, values)
+#             if event == sg.WIN_CLOSED or event == 'Exit':
+#                 break
+#             if event == '-START-':
+#                 dset = dataset_decoder(values['-FILE-'], 3)
+#                 layer = nst.NeuronLayer(3, 35)
+#                 t = nst.Trainer(layer, dataset_decoder('ond_dataset_5rsl.txt', 3))
+#                 t.start_training(0.0001, self._upd_stat)
+#             if event == '-STOP-':
+#                 t.done = True
+
+#         self.presentation_window.close()
+
+
+
+
+
+
+
+
+pres_window = GenerateWindow()
+pres_window.open()
+
+
+
--- one_number_detector/neural_structs.py
+++ one_number_detector/neural_structs.py
@@ -10,14 +10,13 @@ class Neuron():
         if inp_count < 1:
             raise ValueError("У нейрона не может быть меньше одного входа")
         self.input_count: int = inp_count + 1
-
-        self.input_weights: list = inp_weights or [random.random() for inp in range(self.input_count)]
+        self.input_weights: list = inp_weights or [random.random()-random.random() for inp in range(self.input_count)]
         if len(self.input_weights) != (self.input_count):
             raise ValueError("Количество весовых коэффициентов должно удовлетворять формуле <кол-во входов + 1>")
 
 
     def _activation_func(self, x) -> int:
-        if x < self.input_weights[0]:
+        if x <= 0:
             return 0
         else:
             return 1
@@ -25,8 +24,7 @@ class Neuron():
 
     def activation(self, inputs: list[int]) -> int:
         #важно помнить что весовой коэффициент с индексом 0 является пороговым значением и в сумматор не попадает
-        s = sum([inputs[i]*self.input_weights[i] for i in range(1, self.input_count)])
-        print(s)
+        s = sum([inputs[i]*self.input_weights[i] for i in range(1, self.input_count)]) + self.input_weights[0]
         return self._activation_func(s)
 
 
@@ -42,6 +40,16 @@ class NeuronLayer():
             self.neurons = [Neuron(inp_count) for i in range(neurons_count)]
 
 
+    @classmethod
+    def load(cls, path):
+        with open(path, "r") as f:
+            data = f.read()
+        lined_data = data.split('\n')
+        wm = [[np.float64(char) for char in string.split('|')] for string in lined_data]     
+        neuron_count = len(wm[0])
+        inp_count = len(wm) - 1
+        return cls(neuron_count, inp_count, wm)
+
     def get_weight_matrix(self):
         return np.transpose([self.neurons[i].input_weights for i in range(self.neurons_count)])
     
@@ -53,9 +61,81 @@ class NeuronLayer():
 
 
     def activation(self, inputs: list[int]) -> list[int]:
-        return [self.neurons[i].activation(inputs) for i in range(self.neurons_count)]
+        return np.array([self.neurons[i].activation(inputs) for i in range(self.neurons_count)])
+    
+
+    def save_model(self, iteration_count: int, intensity: int):
+        wm = self.get_weight_matrix()
+        data = '\n'.join(['|'.join(string) for string in [[str(i) for i in s] for s in wm]])
+        with open("inp{}_neu{} {}it_{}int.txt".format(self.input_count-1, self.neurons_count, iteration_count, int(intensity)), "w") as f:
+            f.write(data)
 
 
 
-def train(layer: NeuronLayer):
-    pass
+class PerseptronTrainer():
+    def __init__(self, layer: NeuronLayer, dataset: list) -> None:
+        self.dataset = dataset
+        self.net = layer
+        self.iteration = 0
+        self.dataset_fails = [1 for i in dataset]
+
+
+    def _calculate_errors(self, d: list[int]) -> int:
+        i = 0
+        for dev in d:
+            if dev != 0:
+                i += 1
+        return i
+
+
+
+    def start_training(self, intensity: float):
+        print("Train started")
+        while True:
+            set_id = random.randint(0, len(self.dataset)-1)
+            set = self.dataset[set_id]
+            
+
+            set[0].insert(0, -1)
+            inp_v = np.array(set[0])
+            out_v = self.net.activation(inp_v)
+            dout = np.array(set[1]) - out_v
+
+            wm = self.net.get_weight_matrix()
+            for i in range(self.net.input_count):
+                for n in range(self.net.neurons_count):
+                    wm[i][n] += dout[n]*inp_v[i]*intensity
+
+            fails = self._calculate_errors(dout)
+            
+            self.dataset_fails[set_id] = fails
+            
+            
+            self.iteration += 1
+
+            print("Iteration: {}; Set: {}; Out: {}; Fails: {}; Set_fails: {}".format(self.iteration, set_id, out_v, fails, self._calculate_errors(self.dataset_fails)))
+            if self.dataset_fails == 0 or self.iteration == 1000000000:
+                self.net.save_model(self.iteration, intensity)
+                break
+
+
+
+
+def dataset_target_decode(target: int, neurons_count: int):
+    v = [0 for i in range(neurons_count)]
+    v[target-1] = 1
+    return v
+
+
+
+def dataset_decoder(path: str, neurons_count: int):
+    with open(path, 'r') as f:
+        data = f.read()
+    lined_data = data.split('\n')
+    lined_data.pop()
+    return [[[int(x) for x in set[0].split('|')], dataset_target_decode(int(set[1]), neurons_count)] for set in [line.split('>') for line in lined_data]]
+
+l = NeuronLayer(3, 35)
+t = PerseptronTrainer(l, dataset_decoder('ond_dataset_5rsl_1.txt', 3))
+t.start_training(0.03243241)
+
